6a2739dc4bffcdb433ff2e41d1e377294dd7021c
Michael McClellan <mdmcaus@gmail.com>
2019-06-03 09:26:55 -0500

    Add IDO config changes

--- init.el
+++ init.el
@@ -28,6 +28,31 @@ There are two things you can do about this warning:
 (setq initial-scratch-message nil)
 (set-face-attribute 'default nil :height 150)
 
+;; IDO Mode setup
+(require 'ido)
+(ido-mode t)
+(setq ido-file-extensions-order '(".org" ".txt" ".py" ".emacs" ".xml" ".el" ".ini" ".cfg" ".cnf"))
+;; show choices vertically
+(if (version< emacs-version "25")
+  (progn
+    (make-local-variable 'ido-separator)
+    (setq ido-separator "\n"))
+  (progn
+    (make-local-variable 'ido-decorations)
+    (setf (nth 2 ido-decorations) "\n")))
+;; show any name that has the chars you typed
+(setq ido-enable-flex-matching t)
+;; use current pane for newly opened file
+(setq ido-default-file-method 'selected-window)
+;; use current pane for newly switched buffer
+(setq ido-default-buffer-method 'selected-window)
+
+;; stop ido suggestion when doing a save-as
+(define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)
+
+;; big minibuffer height, for ido to show choices vertically
+(setq max-mini-window-height 0.5)
+
 ;; Put backup files neatly away                                                 
 (let ((backup-dir "~/tmp/emacs/backups")
       (auto-saves-dir "~/tmp/emacs/auto-saves/"))
