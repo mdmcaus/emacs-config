e374f8c94aa65b6ba3bdf3953585e8fd73e1138e
Michael McClellan <mdmcaus@gmail.com>
2019-06-06 09:50:11 -0500

    Remove ido and backup config and modularize. Only enable backup.el

--- init.el
+++ init.el
@@ -28,9 +28,15 @@ There are two things you can do about this warning:
 (setq initial-scratch-message nil)
 (set-face-attribute 'default nil :height 150)
 
+;; Load Backup file config
+
+(load "~/.emacs.d/backup.el")
+
 ;; Window movement config
+
 ;; M-o works like C-x o to move to the other window
 (global-set-key (kbd "M-o") 'other-window)
+
 ;; This allows directional selection with Shift:
 ;; S-<left>, S-<right>, S-<up>, S-<down>
 (windmove-default-keybindings)
@@ -42,50 +48,6 @@ There are two things you can do about this warning:
 ;; turn on which-key
 (which-key-mode)
 (setq which-key-popup-type 'minibuffer)
-
-;; IDO Mode setup
-(require 'ido)
-(ido-mode t)
-(setq ido-file-extensions-order '(".org" ".txt" ".py" ".emacs" ".xml" ".el" ".ini" ".cfg" ".cnf"))
-;; show choices vertically
-(if (version< emacs-version "25")
-  (progn
-    (make-local-variable 'ido-separator)
-    (setq ido-separator "\n"))
-  (progn
-    (make-local-variable 'ido-decorations)
-    (setf (nth 2 ido-decorations) "\n")))
-;; show any name that has the chars you typed
-(setq ido-enable-flex-matching t)
-;; use current pane for newly opened file
-(setq ido-default-file-method 'selected-window)
-;; use current pane for newly switched buffer
-(setq ido-default-buffer-method 'selected-window)
-
-;; stop ido suggestion when doing a save-as
-(define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)
-
-;; big minibuffer height, for ido to show choices vertically
-(setq max-mini-window-height 0.5)
-
-;; Put backup files neatly away                                                 
-(let ((backup-dir "~/tmp/emacs/backups")
-      (auto-saves-dir "~/tmp/emacs/auto-saves/"))
-  (dolist (dir (list backup-dir auto-saves-dir))
-    (when (not (file-directory-p dir))
-      (make-directory dir t)))
-  (setq backup-directory-alist `(("." . ,backup-dir))
-        auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
-        auto-save-list-file-prefix (concat auto-saves-dir ".saves-")
-        tramp-backup-directory-alist `((".*" . ,backup-dir))
-        tramp-auto-save-directory auto-saves-dir))
-
-(setq backup-by-copying t    ; Don't delink hardlinks                           
-      delete-old-versions t  ; Clean up the backups                             
-      version-control t      ; Use version numbers on backups,                  
-      kept-new-versions 5    ; keep some new versions                           
-      kept-old-versions 2)   ; and some old ones, too
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
