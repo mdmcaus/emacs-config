789ca813fbd2185d1ed0cea81fdfe8b61cff3ef1
Michael McClellan <mdmcaus@gmail.com>
2019-06-17 14:29:18 -0500

    Add setting to allow direc alternate file which opens file in same buffer

--- init.el
+++ init.el
@@ -110,3 +110,4 @@ There are two things you can do about this warning:
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'dired-find-alternate-file 'disabled nil)
